461949246bc3bcc0f96a1843da3e01936261bb2a
RyomaK <ryooomaaa0413@outlook.com>
2016-10-31 14:03:30 +0900

    edit form

--- app.rb
+++ app.rb
@@ -4,4 +4,14 @@ get "/" do
 	erb:index
 end
 
-
+post "/score" do
+	@semester = params[:semester]
+	@test = params[:test]
+	@school_name = params[:school]
+	@japanese =  params[:japanese]
+	@math =  params[:math]
+	@english =  params[:english]
+	@science =  params[:science]
+	@social =  params[:social]
+	erb:score
+end
